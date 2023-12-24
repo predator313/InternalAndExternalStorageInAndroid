865903aa147d3cdf4449c999eec08285bf35c049
Android Dev <aamirnasa97@gmail.com>
2023-12-24 23:06:24 +0530

    how to use internal storage

---
+++ app/src/main/java/com/aamirashraf/storageinandroid/MainActivity.kt
@@ -0,0 +1,114 @@
+package com.aamirashraf.storageinandroid
+
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.aamirashraf.storageinandroid.adapter.InternalStoragePhotoAdapter
+import com.aamirashraf.storageinandroid.databinding.ActivityMainBinding
+import com.aamirashraf.storageinandroid.model.InternalStoragePhoto
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.IOException
+import java.util.UUID
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var internalStoragePhotoAdapter: InternalStoragePhotoAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        internalStoragePhotoAdapter= InternalStoragePhotoAdapter {
+
+        }
+        //takePhoto is contract
+        val takePhoto=registerForActivityResult(ActivityResultContracts.TakePicturePreview()){
+            val isPrivate=binding.switchPrivate.isChecked
+            if(isPrivate){
+                val isSavedSuccessfully=savePhotoToInternalStorage(UUID.randomUUID().toString(),it!!)
+                if(isSavedSuccessfully){
+                    loadPhotoFromInternalStorageIntoRecyclerView()
+                    Toast.makeText(this@MainActivity,"Saved Successfully",Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this@MainActivity,"Photo not saved",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        //TakePicturePreview gives the bitmap of the photo taken from the camera
+        binding.btnTakePhoto.setOnClickListener {
+//            implementation "androidx.activity:activity-ktx:1.8.2"
+//            implementation 'androidx.fragment:fragment-ktx:1.6.2'
+            //to use launch we need to use these dependencies
+            takePhoto.launch()
+        }
+
+        setUpRecyclerViewInternalStorage()
+        loadPhotoFromInternalStorageIntoRecyclerView()
+
+
+    }
+    private fun savePhotoToInternalStorage(fileName:String,bmp:Bitmap):Boolean{
+        //data related operation should be done in the try and catch block
+        return try {
+            openFileOutput("$fileName.jpg", MODE_PRIVATE).use {stream->
+                //MODE_PRIVATE means the file created here is only access by this application
+                if(!bmp.compress(Bitmap.CompressFormat.JPEG,96,stream)){
+                    throw IOException("can't abe to save bitmap")
+                }
+                return true
+            }
+
+        }catch (e:IOException){
+            e.printStackTrace()
+             false
+        }
+    }
+    private suspend fun loadPhotoFromInternalStorage():List<InternalStoragePhoto>{
+        //here we loads all photo from internal storage so better to use the
+        //suspend function and make all all inside the coroutines async
+        return withContext(Dispatchers.IO){
+            //means withContext we switch from main thread to the background thread
+            val allFiles=filesDir.listFiles()
+            allFiles?.filter { it.canRead() && it.isFile && it.name.endsWith(".jpg") }
+                ?.map {
+                    val bytes=it.readBytes()
+                    val bmp=BitmapFactory.decodeByteArray(bytes,0,bytes.size)
+                    InternalStoragePhoto(it.name,bmp)
+                }?: listOf()  ///if null return empty list
+        }
+    }
+    private fun deletePhotoFromInternalStorage(fileName:String):Boolean{
+        return try {
+            deleteFile(fileName)  //this is the inbuilt function used to delete file
+
+        }
+        catch (e:Exception){
+            e.printStackTrace()
+            false
+        }
+
+    }
+    private fun loadPhotoFromInternalStorageIntoRecyclerView(){
+        lifecycleScope.launch {
+            //means that this coroutines is only active as it parent(activity/fragment)
+            val allPhoto=loadPhotoFromInternalStorage()
+            internalStoragePhotoAdapter.submitList(allPhoto)
+
+        }
+
+    }
+    private fun setUpRecyclerViewInternalStorage()=binding.rvPrivatePhotos.apply {
+        adapter = internalStoragePhotoAdapter
+        layoutManager = StaggeredGridLayoutManager(3, RecyclerView.VERTICAL)
+    }
+}
---
+++ app/src/main/java/com/aamirashraf/storageinandroid/adapter/SharedStoragePhotoAdapter.kt
@@ -0,0 +1,56 @@
+package com.aamirashraf.storageinandroid.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.aamirashraf.storageinandroid.databinding.RvItemsBinding
+import com.aamirashraf.storageinandroid.model.SharedStoragePhoto
+
+class SharedStoragePhotoAdapter(
+    private val onPhotoClick: (SharedStoragePhoto) -> Unit
+) : ListAdapter<SharedStoragePhoto, SharedStoragePhotoAdapter.PhotoViewHolder>(Companion) {
+
+    inner class PhotoViewHolder(val binding: RvItemsBinding): RecyclerView.ViewHolder(binding.root)
+
+    companion object : DiffUtil.ItemCallback<SharedStoragePhoto>() {
+        override fun areItemsTheSame(oldItem: SharedStoragePhoto, newItem: SharedStoragePhoto): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: SharedStoragePhoto, newItem: SharedStoragePhoto): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        return PhotoViewHolder(
+            RvItemsBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        val photo = currentList[position]
+        holder.binding.apply {
+            ivPhoto.setImageURI(photo.contentUri)
+
+            val aspectRatio = photo.width.toFloat() / photo.height.toFloat()
+            ConstraintSet().apply {
+                clone(root)
+                setDimensionRatio(ivPhoto.id, aspectRatio.toString())
+                applyTo(root)
+            }
+
+            ivPhoto.setOnLongClickListener {
+                onPhotoClick(photo)
+                true
+            }
+        }
+    }
+}
